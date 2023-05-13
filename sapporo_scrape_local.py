c31dc69fee1efb5d85a8496eea1d3aa2b49cbbba
Yuki Matsuno <79320522+yukimatsuno@users.noreply.github.com>
2023-05-13 21:06:05 +0900

    Fixed bug. Deleted feature to stop at Error.

--- sapporo_scrape_local.py
+++ sapporo_scrape_local.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 from bs4 import BeautifulSoup
 import requests, random, time, csv, os.path
@@ -6,7 +5,7 @@ import requests, random, time, csv, os.path
 
 def scrape(lastPage):
     list_ = []
-    filename = "/file/path/.csv" 
+    filename = "/path/to/file.csv" 
     # if file exist, append, otherwise create a new file
     for j in range(1, lastPage+1):    
         url = 'https://sapporo-premium2023.jp/shoplist?page=' + str(j)
@@ -18,8 +17,8 @@ def scrape(lastPage):
          #tels = soup.find_all("p", class_="tel")
         len_ = len(shopBlocks)-1
         if len_ != len(addresses):  #or len_ != len(tels):
-            print('Error: length of lists does not match')
-            return
+            print(f'Error: page {j}: length of lists does not match')
+        
         else:
             shopBlocks = shopBlocks[1:]
             for i in range(len_):
@@ -28,21 +27,22 @@ def scrape(lastPage):
                 address = addresses[i].text
                  #tel = tels[i].text
                 dict_ = {"name": name, "category": category, "address": address} #, "tel": tel}
-                list_.append(dict_)   
-
-
-        file_exists = os.path.isfile(filename)
-        keys = list_[0].keys()
-        if file_exists:
-            with open(filename, 'a', newline='') as sapporo:
-                dict_writer = csv.DictWriter(sapporo, keys)
-                dict_writer.writeheader()
-                dict_writer.writerows(list_)
-        if not file_exists:
-            with open(filename, 'a', newline='') as sapporo:
-                dict_writer = csv.DictWriter(sapporo, keys)
-                dict_writer.writerows(list_)
-        print('Completed page '+ str(j))     
+                list_.append(dict_)
+        print('Completed page '+ str(j)) 
         time.sleep(random.randint(3,5)) #次のスクレイぷに進むまで3〜5秒ランダムに待つ
     print("Completed all scrape")
+
+    file_exists = os.path.isfile(filename)
+    keys = list_[0].keys()
+    if file_exists:
+        with open(filename, 'a', newline='') as sapporo:
+            dict_writer = csv.DictWriter(sapporo, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(list_)
+    if not file_exists:
+        with open(filename, 'a', newline='') as sapporo:
+            dict_writer = csv.DictWriter(sapporo, keys)
+            dict_writer.writerows(list_)
     return
+
+scrape(98)
